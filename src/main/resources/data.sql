db379b227e06883316d60200c28504951d5b1ae2
Philip Riecks <mail@philipriecks.de>
2022-06-21 09:04:32 +0200

    upgrade to Spring Boot 2.7.0

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO customer (id, name, joined_at) VALUES (1, 'duke', '2010-01-01 11:00:00+03');
-INSERT INTO customer (id, name, joined_at) VALUES (2, 'andy', '2020-01-01 11:00:00+01');
-INSERT INTO customer (id, name, joined_at) VALUES (3, 'anna', '2021-01-01 11:00:00+04');
-INSERT INTO customer (id, name, joined_at) VALUES (4, 'sandy', '2015-01-01 11:00:00+06');
-INSERT INTO customer (id, name, joined_at) VALUES (5, 'marc', '2001-01-01 11:00:00-06');
+INSERT INTO customer (name, joined_at) VALUES ('duke', '2010-01-01 11:00:00+03');
+INSERT INTO customer (name, joined_at) VALUES ('andy', '2020-01-01 11:00:00+01');
+INSERT INTO customer (name, joined_at) VALUES ('anna', '2021-01-01 11:00:00+04');
+INSERT INTO customer (name, joined_at) VALUES ('sandy', '2015-01-01 11:00:00+06');
+INSERT INTO customer (name, joined_at) VALUES ('marc', '2001-01-01 11:00:00-06');
