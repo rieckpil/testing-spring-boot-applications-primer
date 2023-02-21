3146bcb031fc7f2e9a256d6cf5bdf096852b2122
Philip Riecks <mail@philipriecks.de>
2023-02-21 09:24:22 +0100

    remove deprecated method

--- src/main/java/de/rieckpil/blog/WebSecurityConfig.java
+++ src/main/java/de/rieckpil/blog/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig  {
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
+    http.authorizeHttpRequests()
       .requestMatchers(HttpMethod.GET, "/api/customers").permitAll()
       .requestMatchers(HttpMethod.GET, "/api/customers/*").authenticated()
       .requestMatchers(HttpMethod.GET, "/dashboard").permitAll()
