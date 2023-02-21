5c39eafe40d59d3487b90ec94bdd3110a73ced26
Philip Riecks <mail@philipriecks.de>
2023-02-21 09:23:54 +0100

    migrate to Spring Boot 3.0

--- src/main/java/de/rieckpil/blog/WebSecurityConfig.java
+++ src/main/java/de/rieckpil/blog/WebSecurityConfig.java
@@ -2,23 +2,26 @@ package de.rieckpil.blog;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig  {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-      .mvcMatchers(HttpMethod.GET, "/api/customers").permitAll()
-      .mvcMatchers(HttpMethod.GET, "/api/customers/*").authenticated()
-      .mvcMatchers(HttpMethod.GET, "/dashboard").permitAll()
+      .requestMatchers(HttpMethod.GET, "/api/customers").permitAll()
+      .requestMatchers(HttpMethod.GET, "/api/customers/*").authenticated()
+      .requestMatchers(HttpMethod.GET, "/dashboard").permitAll()
       .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
       .anyRequest().authenticated()
     .and()
     .httpBasic();
+
+    return http.build();
   }
 }
